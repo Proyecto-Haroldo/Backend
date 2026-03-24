b58a9a323c2e04656c8ef3dbba8057407ff621ee
Sonistwo <159084179+Sonistwo@users.noreply.github.com>
2026-03-23 20:43:49 -0500

    Fix: Update register method in AuthController to return AuthResponse type

--- src/main/java/itm/proyectoharoldo/backend/Controllers/AuthController.java
+++ src/main/java/itm/proyectoharoldo/backend/Controllers/AuthController.java
@@ -21,7 +21,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.processRegister(request));
     }
 
