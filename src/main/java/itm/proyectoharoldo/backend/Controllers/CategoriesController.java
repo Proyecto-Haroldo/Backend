fad61424c64ce1c6c2427459df47b49d4a77080a
Sonistwo <159084179+Sonistwo@users.noreply.github.com>
2026-04-25 21:16:23 -0500

    Fix: Update getById method in CategoriesController to enforce non-null constraint on id parameter

--- src/main/java/itm/proyectoharoldo/backend/Controllers/CategoriesController.java
+++ src/main/java/itm/proyectoharoldo/backend/Controllers/CategoriesController.java
@@ -1,10 +1,7 @@
 package itm.proyectoharoldo.backend.Controllers;
 
-import itm.proyectoharoldo.backend.Repositories.CategoryRepository;
 import itm.proyectoharoldo.backend.Services.CategoryService;
-import itm.proyectoharoldo.backend.Models.Category;
 import itm.proyectoharoldo.backend.Models.DTO.CategoryDTO;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
@@ -12,9 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -29,7 +24,7 @@ public class CategoriesController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CategoryDTO> getById(@PathVariable Long id) {
+    public ResponseEntity<CategoryDTO> getById(@PathVariable @NonNull Long id) {
         return ResponseEntity.ok(categoryService.getCategoryById(id));
     }
 
